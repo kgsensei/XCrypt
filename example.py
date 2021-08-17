b32e9ddc3f8f712b4ef9f6264562a633ce7c07c3
kgsensei <jt@kellem.com>
2021-08-16 20:35:10 -0400

    General Update
    -Added Comments to Example.py
    -Fix file names [I got it wrong lol]
    -Modify generation algorithm
    -Improve XCrypt security [Gen, encryption and decryption updates]

--- example.py
+++ example.py
@@ -1,11 +1,31 @@
-import decode
-import encode
+#Import decrypt file [./decrypt.py]-
+#used for decrypting the encrypted data.
+import decrypt
+
+#Import encrypt file [./encrypt.py]-
+#used for encrypting plain text message.
+import encrypt
+
+#Import generate file [./generate.py]-
+#used for generating a valid key file.
 import generate
 
+#Define the message to encrypt and decrypt.
 message=input("Message: ")
-key=generate.makeKey()
-encrypted=encode.encode(key,message)
-decrypted=decode.decode(key,encrypted)
 
+#Build the key and return key file name.
+key=generate.makeKey()
+
+#Encrypt data, pass in the key and message-
+#to encrypt. Return the encryted message.
+encrypted=encrypt.encode(key,message)
+
+#Decrypt data, pass in key and encrypted-
+#message to decrypt. Return plain text message.
+decrypted=decrypt.decode(key,encrypted)
+
+#Print encrypted message, str by default
 print("Encrypted: "+encrypted)
+
+#Print decrypted message, str by default.
 print("Decrypted: "+decrypted)
